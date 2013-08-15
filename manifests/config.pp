22450781f6e21b3fc036ed0d7cff20876eb258b4
Dan Carley <dan.carley@gmail.com>
2013-08-15 13:59:10 +0100

    Remove unused manifest files
    These sneaked in with puppet-module-skeleton and weren't used.

--- manifests/config.pp
+++
@@ -1,6 +0,0 @@
-# == Class gds_accounts::config
-#
-# This class is called from gds_accounts
-#
-class gds_accounts::config {
-}
--- manifests/install.pp
+++
@@ -1,9 +0,0 @@
-# == Class gds_accounts::intall
-#
-class gds_accounts::install {
-  include gds_accounts::params
-
-  package { $gds_accounts::params::package_name:
-    ensure => present,
-  }
-}
